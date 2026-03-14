0478a91ceb0cb98fea4d95feca6b2f6b68bb9867
sun4205 <hanna4205@gmail.com>
2026-03-14 11:47:11 -0400

    add weatherCard

---
+++ src/components/weathercard.tsx
@@ -0,0 +1,17 @@
+import type { weatherData } from "../types/weather";
+
+interface Props {
+  weather: weatherData;
+}
+
+function weatherCard({ weather }: Props) {
+  return (
+    <div>
+      <h2>{weather.name}</h2>
+      <p>temperature:{weather.main.temp}°F</p>
+      <p>weather:{weather.weather[0].main}</p>
+      <p>humidity:{weather.main.humidity}%</p>
+    </div>
+  );
+}
+export default weatherCard;
